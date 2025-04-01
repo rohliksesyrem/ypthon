28ad30806f317e25279b9d0435587a0aec711f75
rohliksesyrem <jiri.rzyman@infis.cz>
2025-04-01 09:30:07 +0200

    Add files via upload

---
+++ BEJVAVALO.py
@@ -0,0 +1,17 @@
+Bejvavalo = ["Píštala","dudy","harfa","dychací harmonika","Kytara","bubny"]
+
+x = len(Bejvavalo)
+print(x)
+
+for x in Bejvavalo:
+    print(x)
+
+bubu = input()
+Bejvavalo.append(bubu)
+
+Bejvavalo.pop(2)
+
+Bejvavalo.reverse()
+
+print(Bejvavalo)
+
